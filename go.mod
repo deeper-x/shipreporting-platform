a1928eae9f01451cf71298a808d3ccbac04b7bae
deeper-x <albertodeprezzo@gmail.com>
2019-11-05 12:10:16 +0100

    template layout configuration

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/shipreporting-platform
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.4.0 // indirect
+require (
+	github.com/gin-contrib/multitemplate v0.0.0-20190914010127-bba2ccfe37ec
+	github.com/gin-gonic/gin v1.4.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-contrib/multitemplate"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello shipreporting")
+	r := gin.Default()
+
+	r.Static("/assets", "./templates/layout/theme/classic/assets/")
+
+	r.HTMLRender = createMultiple()
+
+	r.GET("/home", func(x *gin.Context) {
+		x.HTML(http.StatusOK, "home", gin.H{
+			"ship_name": "Eurocargo XYZ",
+		})
+	})
+
+	r.Run()
+}
+
+func createMultiple() multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+	r.AddFromFiles("home", "templates/layout/theme/classic/default/index.html", "templates/tables/mooring_now.html")
+
+	return r
 }
