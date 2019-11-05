ca42e73870c7ac7b6125204ba666d35839a204ca
deeper-x <albertodeprezzo@gmail.com>
2019-11-05 18:40:01 +0100

    codacy fixes

--- webserver/tmplpaths.go
+++ webserver/tmplpaths.go
@@ -2,5 +2,8 @@ package webserver
 
 import "github.com/shipreporting-platform/utils"
 
+// IndexPath points to html index
 var IndexPath = utils.FullPath("/templates/layout/theme/classic/default/index.html")
+
+// MooringPath points to moored_now table
 var MooringPath = utils.FullPath("/templates/tables/mooring_now.html")
