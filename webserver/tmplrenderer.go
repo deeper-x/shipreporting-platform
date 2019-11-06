66cefc4ffca7200074d92b8914826f4014eb9402
deeper-x <albertodeprezzo@gmail.com>
2019-11-06 12:31:54 +0100

    project structure semplification on template rendering

--- webserver/tmplrenderer.go
+++ webserver/tmplrenderer.go
@@ -2,13 +2,17 @@ package webserver
 
 import (
 	"github.com/gin-contrib/multitemplate"
+	"github.com/shipreporting-platform/utils"
 )
+
+var indexPath = utils.FullPath("/templates/layout/theme/classic/default/index.html")
+var mooringPath = utils.FullPath("/templates/tables/mooring_now.html")
 
 // multiRenderer todo doc
 func multiRenderer() multitemplate.Renderer {
 	r := multitemplate.NewRenderer()
 
-	r.AddFromFiles("mooring_now", IndexPath, MooringPath)
+	r.AddFromFiles("mooring_now", indexPath, mooringPath)
 
 	return r
 }
