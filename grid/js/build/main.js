6ce46b249b7f570844fd0951df9fd71ca0508502
deeper-x <albertodeprezzo@gmail.com>
2019-11-12 12:44:50 +0100

    unittest configuration

--- grid/js/build/main.js
+++ grid/js/build/main.js
@@ -1,27 +1,17 @@
-"use strict";
-function getYear(value) {
+export function getYear(value) {
     return Number(value.split("-")[0]);
 }
-function getMonth(value) {
+export function getMonth(value) {
     return Number(value.split("-")[1]);
 }
-function getDay(value) {
+export function getDay(value) {
     return Number(value.split("-")[2].substr(0, 2));
 }
-function getHour(value) {
+export function getHour(value) {
     var hhmm = value.split("T")[1];
     return Number(hhmm.split(":")[0]);
 }
-function getMinute(value) {
+export function getMinute(value) {
     var hhmm = value.split("T")[1];
     return Number(hhmm.split(":")[1]);
-}
-
-
-module.exports = {
-    'getYear': getYear,
-    'getMonth': getMonth,
-    'getDay': getDay,
-    'getHour': getHour,
-    'getMinute': getMinute,
 }
--- grid/js/src/main.ts
+++ grid/js/src/main.ts
@@ -1,23 +1,22 @@
-function getYear(value: string): number{
+export function getYear(value: string): number{
     return Number(value.split("-")[0]);
 }
 
-function getMonth(value: string): number{
+export function getMonth(value: string): number{
     return Number(value.split("-")[1]);
 }
 
-function getDay(value: string): number{
+export function getDay(value: string): number{
     return Number(value.split("-")[2].substr(0,2));
 }
 
-function getHour(value: string): number{
+export function getHour(value: string): number{
     let hhmm = value.split("T")[1];
     return Number(hhmm.split(":")[0]);
 }
 
-function getMinute(value: string): number{
+export function getMinute(value: string): number{
     let hhmm = value.split("T")[1];
     return Number(hhmm.split(":")[1]);
 }
-
 
