eae41acb324582ef69cdd63f8d6584ae424ebf09
deeper-x <albertodeprezzo@gmail.com>
2020-01-07 18:50:37 +0100

    global refactoring

---
+++ grid/js/src/main.js
@@ -0,0 +1,60 @@
+var firstElem = 0
+var secondElem = 1
+var thirdElem = 2
+var notFound = -1
+
+export var getYear = function (value) {
+  if (value) {
+    return Number(value.split('-')[firstElem])
+  } else {
+    return ''
+  }
+}
+
+export var getMonth = function (value) {
+  if (value) {
+    return Number(value.split('-')[secondElem])
+  } else {
+    return ''
+  }
+}
+
+export var getDay = function (value) {
+  if (value) {
+    return Number(value.split('-')[thirdElem].substr(firstElem, thirdElem))
+  } else {
+    return ''
+  }
+}
+
+export var getHour = function (value) {
+  var hhmm = ''
+  if (value.indexOf('T') !== notFound) {
+    hhmm = value.split('T')[secondElem]
+    return Number(hhmm.split(':')[firstElem])
+  } else if (!value) {
+    return ''
+  } else {
+    hhmm = value.split(' ')[secondElem]
+    return Number(hhmm.split(':')[firstElem])
+  }
+}
+
+export var getMinute = function (value) {
+  var hhmm = ''
+  if (value.indexOf('T') !== notFound) {
+    hhmm = value.split('T')[secondElem]
+    return Number(hhmm.split(':')[secondElem])
+  } else if (!value) {
+    return ''
+  } else {
+    hhmm = value.split(' ')[secondElem]
+    return Number(hhmm.split(':')[secondElem])
+  }
+}
+
+export var buildGrid = function (uri, gridOptions) {
+  agGrid.simpleHttpRequest({ url: uri }).then(function (data) {
+    gridOptions.api.setRowData(data)
+  })
+}
--- grid/js/test/timestamp_manager.js
+++ grid/js/test/timestamp_manager.js
@@ -1,34 +1,35 @@
-var chai = require('chai');
-var moduleMain = require('../build/main.js');
+/* eslint-disable no-undef */
+import { getYear, getMonth, getDay, getHour, getMinute } from '../src/main.js'
+import { expect } from 'chai'
 
-var inputString = "2019-10-31T02:21";
+var inputString = '2019-10-31T02:21'
 
-describe('Timestamp substring', function() {
-    it('should return 2019', function() {
-      chai.expect(moduleMain.getYear(inputString)).to.equal(2019);
-    });
-});
+describe('Timestamp substring', function () {
+  it('should return 2019', function () {
+    expect(getYear(inputString)).to.equal(2019)
+  })
+})
 
-describe('Timestamp substring', function() {
-  it('should return 10', function() {
-    chai.expect(moduleMain.getMonth(inputString)).to.equal(10);
-  });
-});
+describe('Timestamp substring', function () {
+  it('should return 10', function () {
+    expect(getMonth(inputString)).to.equal(10)
+  })
+})
 
-describe('Timestamp substring', function() {
-  it('should return 31', function() {
-    chai.expect(moduleMain.getDay(inputString)).to.equal(31);
-  });
-});
+describe('Timestamp substring', function () {
+  it('should return 31', function () {
+    expect(getDay(inputString)).to.equal(31)
+  })
+})
 
-describe('Timestamp substring', function() {
-  it('should return 2', function() {
-    chai.expect(moduleMain.getHour(inputString)).to.equal(2);
-  });
-});
+describe('Timestamp substring', function () {
+  it('should return 2', function () {
+    expect(getHour(inputString)).to.equal(2)
+  })
+})
 
-describe('Timestamp substring', function() {
-  it('should return 21', function() {
-    chai.expect(moduleMain.getMinute(inputString)).to.equal(21);
-  });
-});
+describe('Timestamp substring', function () {
+  it('should return 21', function () {
+    expect(getMinute(inputString)).to.equal(21)
+  })
+})
