6e6d92ced1ab200848a013a296d98f961558ce18
deeper-x <albertodeprezzo@gmail.com>
2020-01-09 16:39:00 +0100

    moving license key on index

--- grid/js/src/snam.js
+++
@@ -1,2 +0,0 @@
-// Note: we realise that this license key will be visible to others. Given our product is JavaScript, there is little we can do to prevent this.
-export var key = 'Consorzio_SNAMServizi_Nazionali_Avvistamento_Marittimo_Scarl_Consorzio_SNAMServizi_Nazionali_Avvistamento_Marittimo_Scarl_single_1_Devs__30_December_2020_[v2]_MTYwOTI4NjQwMDAwMA==c6f2208a30f28b1b5f2d6324fd885743'
